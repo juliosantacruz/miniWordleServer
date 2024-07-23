5e48245d81e3e1f6f066564082c06e2fec01fafb
aram <julio.santacruzh@gmail.com>
2024-07-23 15:58:43 -0700

    static files test

--- api/urls.py
+++ api/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
     path('', HomeView, name='home' ),
     path('word/', include('word.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
